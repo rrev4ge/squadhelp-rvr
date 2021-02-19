84978d57133f81eb9f8ff94349fa0038c3796885
rrev <68103311+rrev4ge@users.noreply.github.com>
2021-02-19 15:54:44 +0200

    reorganize filethree

--- client/src/test.stories.js
+++
@@ -1,7 +0,0 @@
-import {storiesOf} from '@storybook/react';
-import React from 'react';
-import DashboardHeader from './components/Header/DashboardHeader';
-
-
-storiesOf('DashboardHeader',module)
-    .add('default',()=><DashboardHeader userName='lOX ivanovich'/>);
--- server/src/server/errors/BankDeclineError.js
+++
@@ -1,9 +0,0 @@
-const ApplicationError = require('./ApplicationError');
-
-class BankDeclineError extends ApplicationError{
-  constructor (message) {
-    super(message || 'Bank decline transaction', 403);
-  }
-}
-
-module.exports = BankDeclineError;
