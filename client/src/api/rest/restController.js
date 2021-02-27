4bf21a740ab958d424130d0b48e3b947328fac10
rrev <68103311+rrev4ge@users.noreply.github.com>
2021-02-27 02:32:21 +0200

    bugfix chat routes

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -3,17 +3,17 @@ import http from '../interceptor';
 // view - actionCreator(action) -> rootSaga -> itemSaga -> api
 
 //Chat
-export const getPreviewChat = () => http.post('getPreview');
-export const getDialog = (data) => http.post('getChat', data);
-export const newMessage = (data) => http.post('newMessage', data);
-export const changeChatFavorite = (data) => http.post('favorite', data);
-export const changeChatBlock = (data) => http.post('blackList', data);
-export const getCatalogList = (data) => http.post('getCatalogs', data);
-export const addChatToCatalog = (data) => http.post('addNewChatToCatalog', data);
-export const createCatalog = (data) => http.post('createCatalog', data);
-export const deleteCatalog = (data) => http.post('deleteCatalog', data);
-export const removeChatFromCatalog = (data) => http.post('removeChatFromCatalog', data);
-export const changeCatalogName = (data) => http.post('updateNameCatalog', data);
+export const getPreviewChat = () => http.post('/chat/getPreview');
+export const getDialog = (data) => http.post('/chat/getChat', data);
+export const newMessage = (data) => http.post('/chat/newMessage', data);
+export const changeChatFavorite = (data) => http.post('/chat/favorite', data);
+export const changeChatBlock = (data) => http.post('/chat/blackList', data);
+export const getCatalogList = (data) => http.post('/chat/getCatalogs', data);
+export const addChatToCatalog = (data) => http.post('/chat/addNewChatToCatalog', data);
+export const createCatalog = (data) => http.post('/chat/createCatalog', data);
+export const deleteCatalog = (data) => http.post('/chat/deleteCatalog', data);
+export const removeChatFromCatalog = (data) => http.post('/chat/removeChatFromCatalog', data);
+export const changeCatalogName = (data) => http.post('/chat/updateNameCatalog', data);
 
 //Contests
 export const getCustomersContests = ({offset, limit, contestStatus}) => {
